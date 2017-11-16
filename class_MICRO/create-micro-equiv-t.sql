17db621548b2d8f3df8a6019ee755619e4bd9612
Paul Lynch <plynch@mail.nih.gov>
2017-11-16 17:26:05 -0500

    Generated first draft of MICRO groups; Shennon revised system_groupings.xlsx for new rules about those groupings

--- class_MICRO/create-micro-equiv-t.sql
+++ class_MICRO/create-micro-equiv-t.sql
@@ -1,6 +1,7 @@
 -- Note:  This requires SQLCMD Mode to be turned on
-:setvar equivTable 'MICRO_EQUIV'
-:setvar orgCol 'OrganismGrouper'
+:setvar equivTable "MICRO_EQUIV"
+:setvar orgCol "OrganismGrouper"
+:setvar systemGroupCol "SystemGrouper"
 
 -- Copy source table
 EXEC create_equiv_table $(equivTable), 'MICRO'
@@ -10,10 +11,12 @@ GO
 -- Create modified copies of columns rather than changing originals.
 EXEC dup_column $(equivTable), 'METHOD_TYP', 'METHOD_TYP_REV';
 GO
-UPDATE MICRO_EQUIV set METHOD_TYP_REV=Grouper from MICRO_EQUIV left join MICRO_METHOD
-  on MICRO_EQUIV.METHOD_TYP = MICRO_METHOD.Name where Grouper is not null;
+UPDATE $(equivTable) set METHOD_TYP_REV=Grouper from $(equivTable) left join MICRO_METHOD
+  on $(equivTable).METHOD_TYP = MICRO_METHOD.Name where Grouper is not null;
 
 -- Organism column --
+-- For now Clem says to skip this one.
+/*
 EXEC add_column $(equivTable), $(orgCol)
 GO
 DECLARE @sql nvarchar(500);
@@ -22,17 +25,45 @@ SET @sql='UPDATE MICRO_EQUIV set '+quotename($(orgCol))+
   inner join LOINC_DETAIL_TYPE_1
     on MICRO_EQUIV.LOINC_NUM = LOINC_DETAIL_TYPE_1.LOINC_NUM
   left join MICRO_ORGANISM
-    on LOINC_DETAIL_TYPE_1.COMPONENT_PN = MICRO_ORGANISM.Part';
+    on LOINC_DETAIL_TYPE_1.COMPONENTCORE_PN = MICRO_ORGANISM.Part';
 EXEC(@sql)
+*/
 
 -- Scale column --
+/*  Clem said this one which collapses Ord and Qn is not useful because no
+ *  groups are created.
 EXEC dup_column $(equivTable), 'SCALE_TYP', 'SCALE_TYP_REV';
 GO
 UPDATE MICRO_EQUIV set SCALE_TYP_REV=[Scale-NEW] from MICRO_EQUIV left join MICRO_SCALE
   on MICRO_EQUIV.LOINC_NUM = MICRO_SCALE.LOINC;
+*/
 
 -- System column --
 EXEC dup_column $(equivTable), 'SYSTEM', 'SYSTEM_REV'
 GO
---UPDATE MICRO
+-- Clean up the system grouping table before using it.  (This only has to run
+-- once, but it won't hurt to leave it in case we reload the table.)
+UPDATE MICRO_SYSTEM set $(systemGroupCol) = RTRIM(LTRIM($(systemGroupCol))); -- there was a case of ' "'
+UPDATE MICRO_SYSTEM set $(systemGroupCol) = null where $(systemGroupCol) = '"' or $(systemGroupCol) = '?'
+UPDATE MICRO_SYSTEM set $(systemGroupCol) = 'VM' where $(systemGroupCol) = 'VM?'
+-- Now use the system table to update SYSTEM_REV in the equivalence table.
+UPDATE $(equivTable) set SYSTEM_REV = $(systemGroupCol) from
+ $(equivTable) eqv left join MICRO_SYSTEM on eqv.SYSTEM = MICRO_SYSTEM.Name
+ left join LOINC_DETAIL_TYPE_1 ltd on eqv.LOINC_NUM = ltd.LOINC_NUM
+ left join MICRO_STD_ComponentCore std on ltd.COMPONENTCORE_PN = std.Part
+ where $(systemGroupCol) is not null and (($(systemGroupCol) not in ('AnalRectalStool', 'UrineUrethra'))
+ or (std.STDGrouper is not null));
 
+-- Property column --
+EXEC dup_column $(equivTable), 'PROPERTY', 'PROPERTY_REV'
+GO
+UPDATE $(equivTable) set PROPERTY = 'PrAcncTitr' where PROPERTY in ('PrThr', 'ACnc', 'Titr');
+
+-- Build the equivalance class name
+ALTER TABLE $(equivTable) ADD EQUIV_CLS nvarchar(255);
+GO
+UPDATE $(equivTable) set EQUIV_CLS=CONCAT(COMPONENT,'|',PROPERTY_REV,'|',SYSTEM_REV,'|',METHOD_TYP_REV);
+
+-- Sample output, paritioning by the equivalence class for counts and to remove entries with a count of 1
+select EQUIV_CLS, LOINC_NUM, SYSTEM, SYSTEM_REV, METHOD_TYP, METHOD_TYP_REV
+  from (select *, count(EQUIV_CLS) over(partition by EQUIV_CLS) as CLS_COUNT From $(equivTable)) t  where CLS_COUNT > 1
