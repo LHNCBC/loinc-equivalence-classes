55dfdaa7f33ec08c68d3b424d9028bae77796d09
Paul Lynch <plynch@mail.nih.gov>
2017-12-04 16:21:18 -0500

    UA processing was changed to include groups for the METHOD column

--- class_UA/create-ua-equiv-t.sql
+++ class_UA/create-ua-equiv-t.sql
@@ -17,18 +17,17 @@ GO
 EXEC apply_groups $(equivTable), 'PROPERTY_REV', 'UA_PROPERTY', 'Property_Rev';
 
 -- METHOD column--
-/* No change to METHOD for UA
 EXEC dup_column $(equivTable), 'METHOD_TYP', 'METHOD_REV'
 GO
 EXEC apply_groups $(equivTable), 'METHOD_REV', 'UA_METHOD', 'Method_Rev';
-*/
+
 
 -- Build the equivalance class name
 ALTER TABLE $(equivTable) ADD EQUIV_CLS nvarchar(255);
 GO
-UPDATE $(equivTable) set EQUIV_CLS=CONCAT(COMPONENT,'|',PROPERTY_REV,'|',SYSTEM_REV,'|',METHOD_TYP);
+UPDATE $(equivTable) set EQUIV_CLS=CONCAT(COMPONENT,'|',PROPERTY_REV,'|',SYSTEM_REV,'|',METHOD_REV);
 
 -- Sample output, paritioning by the equivalence class for counts and to remove entries with a count of 1
-select EQUIV_CLS, LOINC_NUM, COMPONENT, PROPERTY, PROPERTY_REV, SYSTEM, SYSTEM_REV, METHOD_TYP
+select EQUIV_CLS, LOINC_NUM, COMPONENT, PROPERTY, PROPERTY_REV, SYSTEM, SYSTEM_REV, METHOD_TYP, METHOD_REV
   from (select *, count(EQUIV_CLS) over(partition by EQUIV_CLS) as CLS_COUNT From $(equivTable)) t  where CLS_COUNT > 1
   order by EQUIV_CLS
