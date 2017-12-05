ca5f6f1e1cdb6ecea2b5630fd8713abe2a8a5857
Paul Lynch <plynch@mail.nih.gov>
2017-12-05 12:43:09 -0500

    Revision to SERO

--- class_SERO/create-sero-equiv-t.sql
+++ class_SERO/create-sero-equiv-t.sql
@@ -27,6 +27,9 @@ EXEC apply_groups $(equivTable), 'PROPERTY_REV', 'SERO_PROPERTY', 'Property_Rev'
 EXEC dup_column $(equivTable), 'METHOD_TYP', 'METHOD_REV'
 GO
 EXEC apply_groups $(equivTable), 'METHOD_REV', 'SERO_METHOD', 'Method_Rev';
+UPDATE $(equivTable) set METHOD_REV='IA*' where
+  (METHOD_REV is null or METHOD_REV = '') and
+  (COMPONENT like '%[+. ]A[bg]' or COMPONENT like '%[+. ]A[bg][+. ]%')
 
 
 -- Build the equivalance class name
@@ -35,6 +38,6 @@ GO
 UPDATE $(equivTable) set EQUIV_CLS=CONCAT(COMPONENT,'|',PROPERTY_REV,'|',SYSTEM_REV,'|',METHOD_REV);
 
 -- Sample output, paritioning by the equivalence class for counts and to remove entries with a count of 1
-select EQUIV_CLS, LOINC_NUM, COMPONENT, PROPERTY, PROPERTY_REV, SYSTEM, SYSTEM_REV, METHOD_TYP, METHOD_REV
+select EQUIV_CLS, LOINC_NUM, COMPONENT, PROPERTY, PROPERTY_REV, SYSTEM, SYSTEM_REV, METHOD_TYP, METHOD_REV, LONG_COMMON_NAME
   from (select *, count(EQUIV_CLS) over(partition by EQUIV_CLS) as CLS_COUNT From $(equivTable)) t  where CLS_COUNT > 1
   order by EQUIV_CLS
