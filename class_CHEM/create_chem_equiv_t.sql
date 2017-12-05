99e4fd3585c193c49441936a385f5064de38b5c2
Paul Lynch <plynch@mail.nih.gov>
2017-12-05 17:47:30 -0500

    Corrections and changes for CHEM

--- class_CHEM/create_chem_equiv_t.sql
+++ class_CHEM/create_chem_equiv_t.sql
@@ -12,7 +12,8 @@ GO
 EXEC apply_groups $(equivTable), 'SYSTEM_REV', $(systemTable), 'System_Rev_1';
 -- Apply System_Rev2 if the COMPONENT is in the oxygen component list
 UPDATE $(equivTable) set SYSTEM_REV = g.System_Rev_2 from $(equivTable) left join
-  $(systemTable) g on COMPONENT=g.Name where COMPONENT in (select Name from CHEM_OXYGEN_COMP)
+  $(systemTable) g on SYSTEM=g.Name where COMPONENT in (select Name from CHEM_OXYGEN_COMP)
+  and g.System_Rev_2 is not null;
 
 -- Property column --
 EXEC dup_column $(equivTable), 'PROPERTY', 'PROPERTY_REV'
@@ -23,6 +24,11 @@ EXEC apply_groups $(equivTable), 'PROPERTY_REV', 'CHEM_PROPERTY', 'Property_Rev'
 EXEC dup_column $(equivTable), 'METHOD_TYP', 'METHOD_REV'
 GO
 EXEC apply_groups $(equivTable), 'METHOD_REV', 'CHEM_METHOD', 'Method_Rev';
+
+-- Time column
+EXEC dup_column $(equivTable), 'TIME_ASPCT', 'TIME_REV'
+GO
+EXEC apply_groups $(equivTable), 'TIME_REV', 'CHEM_TIME', 'Time_Rev';
 
 -- Warning column
 ALTER TABLE $(equivTable) ADD WARNING nvarchar(255);
@@ -36,9 +42,10 @@ UPDATE $(equivTable) set WARNING = g.Comp_Warning from $(equivTable) left join C
 -- Build the equivalance class name
 ALTER TABLE $(equivTable) ADD EQUIV_CLS nvarchar(255);
 GO
-UPDATE $(equivTable) set EQUIV_CLS=CONCAT(COMPONENT,'|',PROPERTY_REV,'|',SYSTEM_REV,'|',METHOD_REV);
+UPDATE $(equivTable) set EQUIV_CLS=CONCAT(COMPONENT,'|',PROPERTY_REV,'|',SYSTEM_REV,'|',METHOD_REV,'|',TIME_REV);
 
 -- Sample output, paritioning by the equivalence class for counts and to remove entries with a count of 1
-select EQUIV_CLS, LOINC_NUM, COMPONENT, PROPERTY, PROPERTY_REV, SYSTEM, SYSTEM_REV, METHOD_TYP, METHOD_REV, LONG_COMMON_NAME, WARNING
+select EQUIV_CLS, LOINC_NUM, COMPONENT, PROPERTY, PROPERTY_REV, SYSTEM, SYSTEM_REV,
+  METHOD_TYP, METHOD_REV, TIME_ASPCT, TIME_REV, LONG_COMMON_NAME, WARNING
   from (select *, count(EQUIV_CLS) over(partition by EQUIV_CLS) as CLS_COUNT From $(equivTable)) t  where CLS_COUNT > 1
   order by EQUIV_CLS
