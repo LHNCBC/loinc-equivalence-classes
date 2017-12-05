1cd40cc7130215e5ee77df38f7aef03dc246b380
Paul Lynch <plynch@mail.nih.gov>
2017-12-05 15:59:56 -0500

    Revision of DRUG/TOX processing

---
+++ class_DrugTox/create-drugtox-equiv-t.sql
@@ -0,0 +1,33 @@
+-- Note:  This requires SQLCMD Mode to be turned on
+:setvar equivTable "DRUGTOX_EQUIV"
+
+-- Copy source table
+EXEC create_equiv_table $(equivTable), 'DRUG/TOX'
+GO
+
+-- System column --
+EXEC dup_column $(equivTable), 'SYSTEM', 'SYSTEM_REV'
+GO
+EXEC apply_groups $(equivTable), 'SYSTEM_REV', 'DRUGTOX_SYSTEM', 'System_Rev';
+
+
+-- Property column --
+EXEC dup_column $(equivTable), 'PROPERTY', 'PROPERTY_REV'
+GO
+EXEC apply_groups $(equivTable), 'PROPERTY_REV', 'DRUGTOX_PROPERTY', 'Property_Rev';
+
+-- METHOD column--
+EXEC dup_column $(equivTable), 'METHOD_TYP', 'METHOD_REV'
+GO
+EXEC apply_groups $(equivTable), 'METHOD_REV', 'DRUGTOX_METHOD', 'Method_Rev';
+
+
+-- Build the equivalance class name
+ALTER TABLE $(equivTable) ADD EQUIV_CLS nvarchar(255);
+GO
+UPDATE $(equivTable) set EQUIV_CLS=CONCAT(COMPONENT,'|',PROPERTY_REV,'|',SYSTEM_REV,'|',METHOD_REV);
+
+-- Sample output, paritioning by the equivalence class for counts and to remove entries with a count of 1
+select EQUIV_CLS, LOINC_NUM, COMPONENT, PROPERTY, PROPERTY_REV, SYSTEM, SYSTEM_REV, METHOD_TYP, METHOD_REV, LONG_COMMON_NAME
+  from (select *, count(EQUIV_CLS) over(partition by EQUIV_CLS) as CLS_COUNT From $(equivTable)) t  where CLS_COUNT > 1
+  order by EQUIV_CLS
