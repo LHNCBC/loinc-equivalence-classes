f5ee6bedab8550853e99cb048e54a517c51fb772
Paul Lynch <plynch@mail.nih.gov>
2018-04-19 18:22:56 -0400

    initial SQL for ABXBACT and CYTO

---
+++ class_CYTO/create-cyto-equiv-t.sql
@@ -0,0 +1,71 @@
+-- Note:  This requires SQLCMD Mode to be turned on
+:setvar equivTable "CYTO_EQUIV"
+
+-- Copy source table
+EXEC create_equiv_table $(equivTable), 'CYTO'
+GO
+
+-- Component column --
+EXEC dup_column $(equivTable), 'COMPONENT', 'COMPONENT_REV'
+GO
+EXEC apply_groups $(equivTable), 'COMPONENT_REV', 'CYTO_COMPONENT', 'Component_Rev';
+
+--- System column --
+EXEC dup_column $(equivTable), 'SYSTEM', 'SYSTEM_REV'
+GO
+EXEC apply_groups $(equivTable), 'SYSTEM_REV', 'CYTO_SYSTEM', 'System_Rev';
+
+-- Property column --
+--EXEC dup_column $(equivTable), 'PROPERTY', 'PROPERTY_REV'
+--GO
+--EXEC apply_groups $(equivTable), 'PROPERTY_REV', 'CYTO_PROPERTY', 'Property_Rev';
+
+-- METHOD column--
+--EXEC dup_column $(equivTable), 'METHOD_TYP', 'METHOD_REV'
+--GO
+--EXEC apply_groups $(equivTable), 'METHOD_REV', 'CYTO_METHOD', 'Method_Rev';
+
+-- Scale column --
+EXEC dup_column $(equivTable), 'SCALE_TYP', 'SCALE_REV'
+GO
+EXEC apply_groups $(equivTable), 'SCALE_REV', 'CYTO_SCALE', 'Scale_Rev';
+
+
+-- Build the equivalance class name
+ALTER TABLE $(equivTable) ADD EQUIV_CLS nvarchar(255);
+GO
+--UPDATE $(equivTable) set EQUIV_CLS=CONCAT(COMPONENT,'|',PROPERTY_REV,'|',SYSTEM_REV,'|',METHOD_REV);
+UPDATE $(equivTable) set EQUIV_CLS=CONCAT(COMPONENT_REV,'|',SYSTEM_REV,'|',SCALE_REV);
+
+-- Sample output, paritioning by the equivalence class for counts and to remove entries with a count of 1
+IF OBJECT_ID('tempdb..#EQUIV_TEMP') IS NOT NULL DROP TABLE #EQUIV_TEMP
+select EQUIV_CLS as heading, * into #EQUIV_TEMP
+  from (select *, count(EQUIV_CLS) over(partition by EQUIV_CLS) as CLS_COUNT From $(equivTable)) t where CLS_COUNT > 1
+  order by EQUIV_CLS
+UPDATE #EQUIV_TEMP set heading = '';
+EXEC dup_column '#EQUIV_TEMP', 'EQUIV_CLS', 'SORT_ORDER'
+GO
+-- Add heading rows with count
+:setvar countField COMPONENT_REV
+insert into #EQUIV_TEMP (heading, $(countField), SORT_ORDER) select EQUIV_CLS,
+  count(EQUIV_CLS), EQUIV_CLS as EQUIV_CLS from #EQUIV_TEMP group by EQUIV_CLS;
+
+-- Add blank row after group
+insert into #EQUIV_TEMP (heading, $(countField), SORT_ORDER) select '', '',
+  EQUIV_CLS + '_BLANKROW' from #EQUIV_TEMP where EQUIV_CLS is not null group by EQUIV_CLS;
+
+-- Set other fields to blank in the heading rows and blank rows except SORT_ORDER (used for sorting)
+-- This avoids having to respecify the fields in the table.
+DECLARE @sql varchar(max)=''
+select @sql= @sql+case when c.name!='heading' and c.name != 'SORT_ORDER' and c.name != '$(countField)' then c.name + '='''',
+' else '' end from tempdb.sys.columns c where object_id =
+object_id('tempdb..#EQUIV_TEMP');
+select @sql = substring(@sql, 1, (len(@sql) - 2)) -- remove last comma
+SET @sql = 'UPDATE #EQUIV_TEMP SET '+@sql + ' where COMPONENT is NULL'
+EXECUTE (@sql)
+
+select heading as 'Heading', EQUIV_CLS, LOINC_NUM, COMPONENT, COMPONENT_REV,-- PROPERTY, PROPERTY_REV,
+ SYSTEM, SYSTEM_REV, --METHOD_TYP, METHOD_REV
+ SCALE_TYP, SCALE_REV, LONG_COMMON_NAME,
+ SORT_ORDER from #EQUIV_TEMP order by SORT_ORDER, heading desc
+
