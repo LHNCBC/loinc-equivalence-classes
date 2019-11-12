d2899dfccf5c8785add98611dd5ee6a10be65cbe
Paul Lynch <plynch@mail.nih.gov>
2019-11-12 11:33:26 -0500

    Moved common JS routines into util.js; stopped setting null molecular weights to empty string, which is not necessary now that that column is a float.  (It shows up blank in the results spreadsheet).

--- util.js
+++ util.js
@@ -1,8 +1,9 @@
-//const mssql = require('mssql');
-const sql = require('mssql/msnodesqlv8');
-const equivConfig = require('../config');
-
-function sqlUtilFactory(pool) {
+/**
+ *  Utility functions used by the various generate_classes.js programs.
+ * @param pool A connection pool retured by
+ *  require('mssql/msnodesqlv8').connect(...).
+ */
+module.exports = function sqlUtilFactory(pool) {
   var rtn = {
     /**
      *  Excecutes the given sql string.
@@ -141,12 +142,18 @@ function sqlUtilFactory(pool) {
      *  Generates the output spreadsheet.
      * @param equivTable the table with the equivalence class data.  The method
      *  sorts and organizes that data in way more suitable for review and presentation.
+     *  Assumption:  equivTable is named in the form "[LOINC_CLASS]_EQUIV" where
+     *  LOINC_CLASS is the same string used in the class-specific
+     *  sub-directories (so we can find the template files).
+     * @param
      */
     equivSpreadsheet: async function(equivTable) {
+      let loincCls = equivTable.slice(0, -6); // remove "_EQUIV"
+      let clsSubDir = 'class_'+loincCls;
       const path = require('path');
       const exceljs = require('exceljs');
       let workbook = new exceljs.Workbook();
-      await workbook.xlsx.readFile(path.join(__dirname, 'results_template.xlsx'));
+      await workbook.xlsx.readFile(path.join(__dirname, clsSubDir, 'results_template.xlsx'));
 
       // There are two sheets, one where we include all of the equivalance classes, and one
       // where we include only those classes with a count > 1.  So, we run through the code below twice.
@@ -162,10 +169,6 @@ function sqlUtilFactory(pool) {
           "UPDATE #EQUIV_TEMP set heading = '';\n"+
           "EXEC dup_column '#EQUIV_TEMP', 'EQUIV_CLS', 'SORT_ORDER'"
         await rtn.query(sql);
-
-        // Set NULL values to blank
-        await rtn.query("UPDATE #EQUIV_TEMP set MOLECULAR_WEIGHT = '' where MOLECULAR_WEIGHT is NULL;\n"+
-          "UPDATE #EQUIV_TEMP set EXAMPLE_UCUM_UNITS = '' where EXAMPLE_UCUM_UNITS is NULL;");
 
         // Add heading rows with count
         let countField = 'SYSTEM_REV';
@@ -232,86 +235,10 @@ function sqlUtilFactory(pool) {
       let date = '' +d.getDate();
       if (date.length < 2)
         date = '0' + date;
-      let loincCls = equivTable.slice(0, -6); // remove "_EQUIV"
       let outputFile = loincCls + '_results-'+d.getFullYear()+'-'+month+'-'+date+'.xlsx';
-      await workbook.xlsx.writeFile(path.join(__dirname, outputFile));
+      await workbook.xlsx.writeFile(path.join(__dirname, clsSubDir, outputFile));
     }
   };
 
   return rtn;
 }
-
-(async function () {
-  let pool = await sql.connect({options: {trustedConnection: true}, server: 'ceb-mssql'});
-  const equivTable = 'DRUGTOX_EQUIV';
-  let util = sqlUtilFactory(pool);
-  let query = util.query;
-  let {dropTable, request, dupColumn, createHatless, applyGroup,
-    applyGroupSkipPatterns, createEquivClasses, addMolecularWeights,
-    equivSpreadsheet} = util;
-  let drugToxConfig = require('./config');
-  try {
-    // Create OXYGEN_COMP table
-    await dropTable('OXYGEN_COMP');
-    await query('CREATE TABLE OXYGEN_COMP (Name nvarchar(255))');
-    let oxygenStrings = equivConfig.COMPONENT.oxygen_related;
-    let promises = oxygenStrings.map(async o2 => {await request().input('o2', o2).
-      query('INSERT INTO OXYGEN_COMP VALUES (@o2)');});
-    await Promise.all(promises);
-
-    // Create the start of the equivalence class table
-    await request().input('tableName', equivTable).input('className', 'DRUG/TOX').execute('create_equiv_table');
-    //let result1 = await pool.request().query('select * from CHEM_METHOD');
-    //console.log(JSON.stringify(result, null, 2));
-
-    // PROPERTY_REV
-    await dupColumn(equivTable, 'PROPERTY', 'PROPERTY_REV');
-    for (let group of Object.keys(drugToxConfig.PROPERTY))
-      await applyGroup(equivTable, 'PROPERTY_REV', drugToxConfig.PROPERTY[group], group);
-
-    // TIME_REV
-    await dupColumn(equivTable, 'TIME_ASPCT', 'TIME_REV');
-    for (let group of Object.keys(drugToxConfig.TIME))
-      await applyGroup(equivTable, 'TIME_REV', drugToxConfig.TIME[group], group);
-
-    // SYSTEM_REV
-    await dupColumn(equivTable, 'SYSTEM', 'SYSTEM_REV');
-    await createHatless(equivTable, 'COMPONENT');
-    for (let group of ["Intravascular - any", "DuodGastricFld", "OcularVitrFld"]) {
-      await applyGroup(equivTable, 'SYSTEM_REV', equivConfig.SYSTEM[group], group);
-    }
-    // For COMPONENTS in the oxygen group, we use different groups.  (Not really
-    // needed for DrugTox, but will run the same code as for CHEM).
-    let condition = 'COMPONENT_HATLESS in (select Name from OXYGEN_COMP)'
-    for (let group of ["Arterial*", "Venous*"])
-      await applyGroup(equivTable, 'SYSTEM_REV', drugToxConfig.SYSTEM[group], group, condition);
-
-    // SCALE_REV
-    await dupColumn(equivTable, 'SCALE_TYP', 'SCALE_REV');
-    for (let group of Object.keys(drugToxConfig.SCALE))
-      await applyGroup(equivTable, 'SCALE_REV', drugToxConfig.SCALE[group], group);
-
-    // METHOD_REV
-    await dupColumn(equivTable, 'METHOD_TYP', 'METHOD_REV');
-    let groupName = 'Method-Other';
-    let skipPatterns = drugToxConfig.METHOD[groupName].skipPatterns;
-    await applyGroupSkipPatterns(equivTable, 'METHOD_REV', skipPatterns, groupName);
-
-    // Equivalance class name
-    await createEquivClasses(equivTable, ['COMPONENT','PROPERTY_REV','TIME_REV',
-      'SYSTEM_REV','SCALE_REV','METHOD_REV'])
-
-    // Molecular weights column
-    await addMolecularWeights(equivTable);
-
-    // Genereate the output
-    await equivSpreadsheet(equivTable);
-  }
-  catch (e) {
-    console.log(e);
-  }
-  finally {
-    await pool.close();
-  }
-})();
-
