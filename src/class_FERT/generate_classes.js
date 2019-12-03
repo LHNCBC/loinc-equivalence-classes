ebaf698125b7c4ee05fafd1dde2f31716d89e8b6
Paul Lynch <plynch@mail.nih.gov>
2019-12-03 17:23:34 -0500

    Some cleanup of the documentation file, plus a change to FERT (per Clem)

--- src/class_FERT/generate_classes.js
+++ src/class_FERT/generate_classes.js
@@ -1,16 +1,12 @@
 // A program to generate the equivalence classes for the LOINC class (loincCls) below.
 const loincCls = 'FERT';
 
-require('../util').genTableAndResults(loincCls, ['COMPONENT', 'PROPERTY_REV',
+require('../util').genTableAndResults(loincCls, ['COMPONENT',
   'SCALE_REV'], async function(equivTable, util) {
 
-  const clsConfig = require('./config');
   const equivConfig = require('../config');
 
   let {dupAndApplyGroups} = util;
-
-  // PROPERTY_REV
-  await dupAndApplyGroups(equivTable, 'PROPERTY', clsConfig.PROPERTY);
 
   // SCALE_REV
   let scaleGroup = "OrdNomNarDoc";
